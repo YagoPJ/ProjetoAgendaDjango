95897c05d2ef11b39240460dca43167fa8600357
Kaio <projetosyago@gmail.com>
2025-08-17 11:26:17 -0300

    Alterações Teste

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    #list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
